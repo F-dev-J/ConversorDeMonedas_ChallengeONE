8c160997818c301377a2c385d668de237dad0f8f
Cufa <juarezfacundo39@gmail.com>
2026-02-28 03:04:21 -0300

    pude conectar la api a la aplicacion, hacer una conversion de una moneda a otra, en el siguiente commit, ya agregare las clases constructoras para generalizar la conversion a unidades

---
+++ src/ConsumirAPI.java
@@ -0,0 +1,34 @@
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsumirAPI {
+    public Monedas moneda(String tipoDeCambio){
+        String key = "83f765305db1d83cf7555ef9";
+        URI direccion = URI.create("https://v6.exchangerate-api.com/v6/"+key+"/latest/"+tipoDeCambio);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(direccion)
+                .build();
+        try{
+            HttpResponse<String> response = null;
+            response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            return new Gson().fromJson(response.body(), Monedas.class);
+        }
+        catch (IOException | InterruptedException e){
+            throw new RuntimeException(e);
+        }
+
+
+    }
+    public double obtenerTasa(String base, String destino) {
+        Monedas datos = moneda(base);
+        return datos.conversion_rates().get(destino);
+    }
+
+}
--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,32 @@
+
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) {
-        System.out.println("hello word");
+        System.out.println("""
+               *************************
+                Conversor de bases monetarias:
+                1) Pesos argentinos--->Dolares
+                2) Dolares----->Pesos argentinos
+                Elija el tipo de cambio:
+                ************************""" );
+        Scanner lectura= new Scanner(System.in);
+        int opcion = lectura.nextInt();
+        ConsumirAPI consulta = new ConsumirAPI();
+
+        switch(opcion){
+            case 1:
+                System.out.println("------------Pesos argentinos a dolares estadounidense--------");
+                System.out.println("ingrese la cantidad de ARS a convertir:");
+                double pesos = lectura.nextDouble();
+                double tasa = consulta.obtenerTasa("USD", "ARS");
+                double dolares = pesos / tasa;
+                DecimalFormat df = new DecimalFormat("#.00");
+                System.out.println("Resultado: " + df.format(dolares) + " USD");
+                break;
+        }
+
     }
 }
